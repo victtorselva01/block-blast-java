aa1718db66277ae7836ccd209f6d1b4e64e0a3ff
Victor S. <victtors.web@hotmail.com>
2026-09-02 13:31:01 +0200

    Implementa tabuleiro com matriz int[8][8]

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -1,11 +1,18 @@
 public class Tabuleiro {
     public static void main(String[] args) {
-        int linhas = 8;
-        int colunas = 9;
+        int[][] tabuleiro = new int[8][8];
 
-        for (int linha = 0; linha < linhas; linha++) {
-            for (int coluna = 0; coluna < colunas; coluna++) {
-                System.out.print(". ");
+        tabuleiro[0][0] = 1;
+        tabuleiro[2][3] = 1;
+        tabuleiro[7][7] = 1;
+
+        for (int linha = 0; linha < tabuleiro.length; linha++) {
+            for (int coluna = 0; coluna < tabuleiro[linha].length; coluna++) {
+                if (tabuleiro[linha][coluna] == 1) {
+                    System.out.print("# ");
+                } else {
+                    System.out.print(". ");
+                }
             }
             System.out.println();
         }
