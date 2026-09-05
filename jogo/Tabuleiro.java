0102f83f1805ae9fbc3e1dbe585859fc7d101a43
Victor S. <victtors.web@hotmail.com>
2026-09-05 16:17:22 +0200

    Remove Tabuleiro.java duplicado da raiz e implementa preenchimento de linha

--- jogo/Tabuleiro.java
+++ jogo/Tabuleiro.java
@@ -6,6 +6,10 @@ public class Tabuleiro {
         tabuleiro[2][3] = 1;
         tabuleiro[7][7] = 1;
 
+        for (int coluna = 0; coluna < 8; coluna++) {
+            tabuleiro[4][coluna] = 1;
+        }
+
         for (int linha = 0; linha < tabuleiro.length; linha++) {
             for (int coluna = 0; coluna < tabuleiro[linha].length; coluna++) {
                 if (tabuleiro[linha][coluna] == 1) {
